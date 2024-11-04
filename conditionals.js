6e67b33ec11b9f38760d212368cb7205d9d2c4c8
tchowdhuryf <tasnim.chowdhury837@gmail.com>
2024-11-04 18:34:01 -0500

    excercise

---
+++ conditionals.js
@@ -0,0 +1,41 @@
+// Write a nested if...else statement.
+// Declare a variable called num.
+// Add an if...else statement that checks if num is positive & greater than 100.
+// Add another statement that checks if num is positive but less than 100.
+// Add a final statement to check if num is negative.
+
+// let num;
+
+// if (num > 100 && num > 0) {
+//     console.log("The number is greater than 100 and is positive")
+// } else if 
+
+
+
+
+
+// Write an if..else statement for the following requirements:
+
+// If a learner gets 90 or higher: console.log("A")
+// If a learner get 80 or above: console.log("B")
+// If a learner get 70 or above: console.log("C")
+// If a learner get 55 or above: console.log("D")
+// Any grade lower than 55: console.log("F")
+
+let num = 101;
+
+if (num >= 90) {
+    console.log("A")
+}
+else if(num  >= 80) {
+    console.log("B")
+}
+else if(num >= 70) {
+    console.log("C")
+}
+else if(num >= 55) {
+    console.log("D")
+}
+else {
+    console.log("F")
+}
--- index.mjs
+++ index.mjs
@@ -18,7 +18,7 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isOver25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
